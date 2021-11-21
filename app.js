5394356e46e93b934553251cbeb1904aeb33c29f
Luis Pineda <lspnd00@gmail.com>
2021-11-20 23:50:00 -0500

    counter on click example

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-    };
+    }
   },
-});
+  methods: {
+    add(num) {
+      this.counter = this.counter + num
+    },
+    subtract(num) {
+      this.counter = this.counter - num
+    },
+  },
+})
 
-app.mount('#events');
+app.mount('#events')
